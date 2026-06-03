784c47027a77d734df061dee5a903f1aa6577479
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-02 22:06:49 -0400

    Crea implementacion de repositorio BorrameRepositoryImpl

---
+++ app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/data/repository/BorrameRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package edu.ucne.adonis_mercado_ap2_p1.data.repository
+
+import edu.ucne.adonis_mercado_ap2_p1.domain.repository.BorrameRepository
+import javax.inject.Inject
+
+class BorrameRepositoryImpl @Inject constructor(
+
+) : BorrameRepository {
+
+}
