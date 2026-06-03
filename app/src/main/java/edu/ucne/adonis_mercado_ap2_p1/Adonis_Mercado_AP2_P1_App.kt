be3f7bcbdabe9735c341a8ff74a784fd40c73db4
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-02 21:32:06 -0400

    Crea clase de la aplicacion Adonis_Mercado_AP2_P1_App

---
+++ app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/Adonis_Mercado_AP2_P1_App.kt
@@ -0,0 +1,7 @@
+package edu.ucne.adonis_mercado_ap2_p1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Adonis_Mercado_AP2_P1_App : Application()
