9237b7621bf9fcebf12b5f4496925b6a7d41cd81
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-02 22:07:37 -0400

    Crea viewmodels vacios

---
+++ app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/presentation/edit/EditBorrameViewModel.kt
@@ -0,0 +1,11 @@
+package edu.ucne.adonis_mercado_ap2_p1.presentation.edit
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class EditBorrameViewModel @Inject constructor(
+
+): ViewModel() {
+}
---
+++ app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/presentation/list/ListBorrameViewModel.kt
@@ -0,0 +1,11 @@
+package edu.ucne.adonis_mercado_ap2_p1.presentation.list
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ListBorrameViewModel @Inject constructor(
+
+): ViewModel() {
+}
