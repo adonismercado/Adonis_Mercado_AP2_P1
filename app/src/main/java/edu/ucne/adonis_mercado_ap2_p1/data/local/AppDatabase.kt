f57ed318494c247e5e08249f56bd9e5b25b944c7
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-10 18:37:37 -0400

    Crea archivo de base de datos AppDatabase

--- app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/data/local/AppDatabase.kt
+++ app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/data/local/AppDatabase.kt
@@ -1,4 +1,19 @@
 package edu.ucne.adonis_mercado_ap2_p1.data.local
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.adonis_mercado_ap2_p1.data.local.dao.AmonestacionDao
+import edu.ucne.adonis_mercado_ap2_p1.data.local.entity.AmonestacionEntity
+
+@Database(
+    entities = [
+        AmonestacionEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+
+
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun amonestacionDao(): AmonestacionDao
 }
