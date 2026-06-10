938108c51e50bc61589d88bad70a97f291c9a189
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-10 18:38:47 -0400

    Crea modelo Amonestacion

---
+++ app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/domain/model/Amonestacion.kt
@@ -0,0 +1,8 @@
+package edu.ucne.adonis_mercado_ap2_p1.domain.model
+
+data class Amonestacion (
+    val amonestacionId: Int = 0,
+    val nombres: String,
+    val razon: String,
+    val monto: String,
+)
--- app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/domain/model/Borrame.kt
+++
@@ -1,5 +0,0 @@
-package edu.ucne.adonis_mercado_ap2_p1.domain.model
-
-class Borrame {
-    val borrameId: Int = 0
-}
