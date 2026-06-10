2db542dec21ba9169d4387a239d1d923476c878f
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-10 19:16:00 -0400

    Completa screen de editar amonestaciones EditAmonestacionScreen

--- app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/presentation/edit/EditAmonestacionScreen.kt
+++ app/src/main/java/edu/ucne/adonis_mercado_ap2_p1/presentation/edit/EditAmonestacionScreen.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
@@ -24,9 +24,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -34,15 +36,22 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditBorrameScreen (
-    viewModel: EditBorrameViewModel = hiltViewModel(),
+fun EditAmonestacionScreen (
+    viewModel: EditAmonestacionViewModel = hiltViewModel(),
     onBack: () -> Unit
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+
+    if (state.saved || state.deleted) {
+        SideEffect {
+            onBack()
+        }
+    }
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
-                title = { Text(if (state.isNew) "Crear Borrame" else "Editar Borrame") },
+                title = { Text(if (state.isNew) "Crear Amonestacion" else "Editar Amonestacion") },
                 navigationIcon = {
                     IconButton(
                         onClick = onBack
@@ -63,10 +72,31 @@ fun EditBorrameScreen (
                 .padding(8.dp)
         ) {
             OutlinedTextField(
-                value = state.dato,
-                onValueChange = {},
-                label = { Text("Dato:") },
-                modifier = Modifier.fillMaxWidth()
+                value = state.nombres,
+                onValueChange = { viewModel.onEvent(EditAmonestacionUiEvent.NombreChanged(it)) },
+                label = { Text("Nombres:") },
+                modifier = Modifier.fillMaxWidth(),
+                isError = state.nombresError != null,
+                supportingText = { state.nombresError?.let { Text(it) }}
+            )
+
+            OutlinedTextField(
+                value = state.razon,
+                onValueChange = { viewModel.onEvent(EditAmonestacionUiEvent.RazonChanged(it)) },
+                label = { Text("Nombres:") },
+                modifier = Modifier.fillMaxWidth(),
+                isError = state.razonError != null,
+                supportingText = { state.razonError?.let { Text(it) }}
+            )
+
+            OutlinedTextField(
+                value = state.monto,
+                onValueChange = { viewModel.onEvent(EditAmonestacionUiEvent.MontoChanged(it)) },
+                label = { Text("Monto:") },
+                modifier = Modifier.fillMaxWidth(),
+                isError = state.montoError != null,
+                supportingText = { state.montoError?.let { Text(it) }},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
 
             Spacer(modifier = Modifier.weight(1f))
@@ -77,7 +107,7 @@ fun EditBorrameScreen (
             ) {
                 if (!state.isNew) {
                     Button(
-                        onClick = { },
+                        onClick = { viewModel.onEvent(EditAmonestacionUiEvent.Delete) },
                         modifier = Modifier.weight(1f),
                         colors = ButtonDefaults.buttonColors(
                             containerColor = MaterialTheme.colorScheme.error
@@ -90,7 +120,7 @@ fun EditBorrameScreen (
                 }
 
                 Button(
-                    onClick = { },
+                    onClick = { viewModel.onEvent(EditAmonestacionUiEvent.Save) },
                     modifier = Modifier.weight(1f)
                 ) {
                     Icon(Icons.Default.Save, contentDescription = null)
@@ -104,8 +134,8 @@ fun EditBorrameScreen (
 
 @Preview(showBackground = true)
 @Composable
-fun EditBorramePreview() {
-    EditBorrameScreen(
+fun EditAmonestacionPreview() {
+    EditAmonestacionScreen(
         onBack = {}
     )
 }
